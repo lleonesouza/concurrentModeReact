f612d70606c9490cef22c52ae8314ecb9d39838d
lleonesouza <aliencode.host@gmail.com>
2020-01-15 23:21:06 -0300

    add style to components

--- src/components.js
+++ src/components.js
@@ -1,24 +1,30 @@
-import React from 'react'
+import React from "react";
 
-function Number({resource}) {
-    const number = resource.number.read()
-    return (
-        <div>
-              {number}
-        </div>
-    )
+function Number({ resource }) {
+  const number = resource.number.read();
+  return <p>{number}</p>;
 }
 
-function User({resource}) {
-    const user = resource.person.read()
-    return (
-        <div style={{border: '5px black solid'}} >
-            {user.name.title}
-            <br/>
-            <img src={user.picture.medium} alt={user.name.title}/>
-        </div>
-    )
+function User({ resource }) {
+  const user = resource.person.read();
+  return (
+    <div
+      style={{
+        display: 'flex',
+        border: "5px black solid",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center"
+      }}
+    >
+      {user.name.title}. {user.name.first} {user.name.last}
+  
+      <img
+        src={user.picture.medium}
+        alt={user.name.title}
+      />
+    </div>
+  );
 }
 
-
-export {Number, User}
+export { Number, User };
